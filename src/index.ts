963f73b6e63e77eb9d37e2e1b4f26441491886b3
tharathep.dev <tharathep.dev@gmail.com>
2025-06-14 20:17:09 +0700

    allow front end url to call api

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import authMiddleware from "./middlewares/auth";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000","https://re-brain.vercel.app/"];
 
 app.use(
   cors({
